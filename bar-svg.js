5252f46a5f1159834572ae153383f6e3c1f8e94f
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-15 02:38:12 -0700

    showing a bar chart

--- bar-svg.js
+++ bar-svg.js
@@ -1,17 +1,20 @@
 var data = [4, 8, 15, 16, 23, 42];
-          
+var scaling = 1;          
+var max_height = scaling * d3.max(data);
+
 var chart = d3.select("body").append("svg")
-     .attr("class", "chart")
-     .attr("width", 420)
-     .attr("height", 20 * data.length);
+    .attr("class", "chart")
+    .attr("width", 20 * data.length)
+    .attr("height", max_height);
 
 var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+    .domain([0, 420])
+    .range([0, d3.max(data)]);
 
 chart.selectAll("rect")
-     .data(data)
-   .enter().append("rect")
-     .attr("y", function(d, i) { return i * 20; })
-     .attr("width", x)
-     .attr("height", 20);
+    .data(data)
+    .enter().append("rect")
+    .attr("x", function(d, i) { return i * 20; })
+    .attr("y", function(d) { return max_height - (scaling * d);})
+    .attr("height", function(d, i) { return d * scaling; })
+    .attr("width", 20);
