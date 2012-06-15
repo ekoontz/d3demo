2d61ad28a09358e5634dbe0fc3d57f046654c57f
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-15 01:30:47 -0700

    renaming

--- population.js
+++ population.js
@@ -1,10 +1,10 @@
 var width = 600,
     height = 500;
 
-var x = d3.scale.linear()
+var x_axis = d3.scale.linear()
     .range([0, width]);
 
-var y = d3.scale.linear()
+var y_axis = d3.scale.linear()
     .range([0, height - 40]);
 
 // An SVG element with a bottom-right origin.
@@ -13,7 +13,7 @@ var svg = d3.select("#chart").append("svg")
     .attr("height", height)
     .style("padding-right", "30px")
   .append("g")
-    .attr("transform", "translate(" + x(1) + "," + (height - 20) + ")scale(-1,-1)");
+    .attr("transform", "translate(" + x_axis(1) + "," + (height - 20) + ")scale(-1,-1)");
 
 // A sliding container to hold the bars.
 var body = svg.append("g")
@@ -26,7 +26,7 @@ var rules = svg.append("g");
 var title = svg.append("text")
     .attr("class", "title")
     .attr("dy", ".71em")
-    .attr("transform", "translate(" + x(1) + "," + y(1) + ")scale(-1,-1)")
+    .attr("transform", "translate(" + x_axis(1) + "," + y_axis(1) + ")scale(-1,-1)")
     .text(2000);
 
 d3.csv("./population.csv", function(data) {
@@ -46,15 +46,15 @@ d3.csv("./population.csv", function(data) {
       current_year = max_year;
 
   // Update the scale domains.
-  x.domain([0, max_age + 5]);
-  y.domain([0, d3.max(data, function(d) { return d.people; })]);
+  x_axis.domain([0, max_age + 5]);
+  y_axis.domain([0, d3.max(data, function(d) { return d.people; })]);
 
   // Add rules to show the population values.
   rules = rules.selectAll(".rule")
-      .data(y.ticks(10))
+      .data(y_axis.ticks(10))
     .enter().append("g")
       .attr("class", "rule")
-      .attr("transform", function(d) { return "translate(0," + y(d) + ")"; });
+      .attr("transform", function(d) { return "translate(0," + y_axis(d) + ")"; });
 
   rules.append("line")
       .attr("x2", width);
@@ -69,18 +69,18 @@ d3.csv("./population.csv", function(data) {
   var years = body.selectAll("g")
       .data(d3.range(min_year - max_age, max_year + 5, 5))
     .enter().append("g")
-      .attr("transform", function(d) { return "translate(" + x(max_year - d) + ",0)"; });
+      .attr("transform", function(d) { return "translate(" + x_axis(max_year - d) + ",0)"; });
 
   years.selectAll("rect")
       .data(d3.range(2))
     .enter().append("rect")
       .attr("x", 1)
-      .attr("width", x(5) - 2)
+      .attr("width", x_axis(5) - 2)
       .attr("height", 1e-6);
 
   years.append("text")
       .attr("y", -6)
-      .attr("x", -x(5) / 2)
+      .attr("x", -x_axis(5) / 2)
       .attr("transform", "rotate(180)")
       .attr("text-anchor", "middle")
       .style("fill", "#fff")
@@ -91,7 +91,7 @@ d3.csv("./population.csv", function(data) {
       .data(d3.range(0, max_age + 5, 5))
     .enter().append("text")
       .attr("text-anchor", "middle")
-      .attr("transform", function(d) { return "translate(" + (x(d) + x(5) / 2) + ",-4)scale(-1,-1)"; })
+      .attr("transform", function(d) { return "translate(" + (x_axis(d) + x_axis(5) / 2) + ",-4)scale(-1,-1)"; })
       .attr("dy", ".71em")
       .text(String);
 
@@ -112,6 +112,6 @@ d3.csv("./population.csv", function(data) {
         .data(function(d) { return data[current_year][d] || [0, 0]; })
       .transition()
         .duration(750)
-        .attr("height", y);
+        .attr("height", y_axis);
   }
 });
