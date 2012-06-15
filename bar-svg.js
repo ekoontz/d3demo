f8107436c16e47f3310fedbc7c6916c3c41e6744
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-15 02:57:47 -0700

    make it more HBase-specific

--- bar-svg.js
+++ bar-svg.js
@@ -1,20 +1,25 @@
-var data = [4, 8, 15, 16, 23, 42];
-var scaling = 1;          
-var max_height = scaling * d3.max(data);
+var region_load_report = 
+    [
+	{"hostname":"rs1","regions":4},
+	{"hostname":"rs2","regions":10},
+	{"hostname":"rs3","regions":2},
+	{"hostname":"rs4","regions":3}
+    ];
 
-var chart = d3.select("body").append("svg")
+var vertical_scaling = 10;          
+var bar_width = 30;
+var max_height = vertical_scaling * d3.max(region_load_report,
+					   function(regionserver) { return regionserver.regions});
+
+var chart = d3.select("#rsload").append("svg")
     .attr("class", "chart")
-    .attr("width", 20 * data.length)
+    .attr("width", bar_width * region_load_report.length)
     .attr("height", max_height);
 
-var x = d3.scale.linear()
-    .domain([0, 420])
-    .range([0, d3.max(data)]);
-
 chart.selectAll("rect")
-    .data(data)
+    .data(region_load_report)
     .enter().append("rect")
-    .attr("x", function(d, i) { return i * 20; })
-    .attr("y", function(d) { return max_height - (scaling * d);})
-    .attr("height", function(d, i) { return d * scaling; })
-    .attr("width", 20);
+    .attr("x", function(regionserver, i) { return i * bar_width; })
+    .attr("y", function(regionserver) { return max_height - (vertical_scaling * regionserver.regions);})
+    .attr("height", function(regionserver, i) { return regionserver.regions * vertical_scaling; })
+    .attr("width", bar_width);
