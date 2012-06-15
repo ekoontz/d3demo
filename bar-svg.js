b45b10b4f386f7e0aecfc3681d1f4b3975a97ec3
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-15 04:04:19 -0700

    working on ticks

--- bar-svg.js
+++ bar-svg.js
@@ -1,6 +1,6 @@
 var region_load_report = 
     [
-	{"hostname":"rs1","regions":4},
+	{"hostname":"rs1","regions":5},
 	{"hostname":"rs2","regions":10},
 	{"hostname":"rs3","regions":2},
 	{"hostname":"rs4","regions":3}
@@ -16,14 +16,19 @@ var chart = d3.select("#rsload").append("svg")
     .attr("width", bar_width * region_load_report.length)
     .attr("height", max_height);
 
+var x = function(regionserver, i) { return i * bar_width; }
+var y = function(regionserver) { return max_height - (vertical_scaling * regionserver.regions);}
+
+// 1. the actual bar chart
 chart.selectAll("rect")
     .data(region_load_report)
     .enter().append("rect")
-    .attr("x", function(regionserver, i) { return i * bar_width; })
-    .attr("y", function(regionserver) { return max_height - (vertical_scaling * regionserver.regions);})
+    .attr("x", x)
+    .attr("y", y)
     .attr("height", function(regionserver, i) { return regionserver.regions * vertical_scaling; })
     .attr("width", bar_width);
 
+// 2. show regionserver hostname.
 chart.selectAll("text")
     .data(region_load_report)
     .enter().append("text")
@@ -33,3 +38,5 @@ chart.selectAll("text")
     .attr("dy", ".35em") // vertical-align: middle
     .attr("text-anchor", "middle")
     .text(function(regionserver){ return regionserver.hostname;});
+
+// 3. ticks
