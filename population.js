5a1ef548b3896cd4fce40f4079472285feb0f3d5
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-15 01:14:27 -0700

    failed attempt to minimize

--- population.js
+++ population.js
@@ -27,104 +27,72 @@ var title = svg.append("text")
     .attr("class", "title")
     .attr("dy", ".71em")
     .attr("transform", "translate(" + x(1) + "," + y(1) + ")scale(-1,-1)")
-    .text(2000);
+    .text("hello");
 
 d3.csv("./population.csv", function(data) {
 
-  // Convert strings to numbers.
-  data.forEach(function(d) {
-    d.people = +d.people;
-    d.year = +d.year;
-    d.age = +d.age;
-  });
+    // Convert strings to numbers.
+    data.forEach(function(d) {
+	d.people = +d.people;
+	d.year = +d.year;
+	d.age = +d.age;
+    });
+    
+    var min_age = 0,
+    max_age = d3.max(data, function(d) { return d.age; }),
+    min_year = d3.min(data, function(d) { return d.year; }),
+    max_year = d3.max(data, function(d) { return d.year; }),
+    year = max_year;
+    
 
-  // Compute the extent of the data set in age and years.
-  var age0 = 0,
-      age1 = d3.max(data, function(d) { return d.age; }),
-      year0 = d3.min(data, function(d) { return d.year; }),
-      year1 = d3.max(data, function(d) { return d.year; }),
-      year = year1;
+    x.domain([0, max_age + 5]);
+    y.domain([0, d3.max(data, function(d) {return d.people;})]);
+   
+    rules = rules.selectAll(".rule")
+	.data(y.ticks(10))
+	.enter().append("g")
+	.attr("class", "rule")
+	.attr("transform", function(d) { return "translate(0," + y(d) + ")"; });
 
-  // Update the scale domains.
-  x.domain([0, age1 + 5]);
-  y.domain([0, d3.max(data, function(d) { return d.people; })]);
-
-  // Add rules to show the population values.
-  rules = rules.selectAll(".rule")
-      .data(y.ticks(10))
-    .enter().append("g")
-      .attr("class", "rule")
-      .attr("transform", function(d) { return "translate(0," + y(d) + ")"; });
-
-  rules.append("line")
+    rules.append("line")
       .attr("x2", width);
 
-  rules.append("text")
-      .attr("x", 6)
-      .attr("dy", ".35em")
-      .attr("transform", "rotate(180)")
-      .text(function(d) { return Math.round(d / 1e6) + "M"; });
-
-  // Add labeled rects for each birthyear.
-  var years = body.selectAll("g")
-      .data(d3.range(year0 - age1, year1 + 5, 5))
-    .enter().append("g")
-      .attr("transform", function(d) { return "translate(" + x(year1 - d) + ",0)"; });
-
-  years.selectAll("rect")
-      .data(d3.range(2))
-    .enter().append("rect")
-      .attr("x", 1)
-      .attr("width", x(5) - 2)
-      .attr("height", 1e-6);
-
-  years.append("text")
-      .attr("y", -6)
-      .attr("x", -x(5) / 2)
-      .attr("transform", "rotate(180)")
-      .attr("text-anchor", "middle")
-      .style("fill", "#fff")
-      .text(String);
-
-  // Add labels to show the age.
-  svg.append("g").selectAll("text")
-      .data(d3.range(0, age1 + 5, 5))
-    .enter().append("text")
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) { return "translate(" + (x(d) + x(5) / 2) + ",-4)scale(-1,-1)"; })
-      .attr("dy", ".71em")
-      .text(String);
-
-  // Nest by year then birthyear.
-  data = d3.nest()
-      .key(function(d) { return d.year; })
-      .key(function(d) { return d.year - d.age; })
-      .rollup(function(v) { return v.map(function(d) { return d.people; }); })
-      .map(data);
-
-  // Allow the arrow keys to change the displayed year.
-  d3.select(window).on("keydown", function() {
-    switch (d3.event.keyCode) {
-      case 37: year = Math.max(year0, year - 10); break;
-      case 39: year = Math.min(year1, year + 10); break;
-    }
-    redraw();
-  });
-
-  redraw();
-
-  function redraw() {
-    if (!(year in data)) return;
-    title.text(year);
-
-    body.transition()
-        .duration(750)
-        .attr("transform", function(d) { return "translate(" + x(year - year1) + ",0)"; });
+    rules.append("text")
+	.attr("x", 6)
+	.attr("dy", ".35em")
+	.attr("transform", "rotate(180)")
+	.text(function(d) { return Math.round(d / 1e6) + "M"; });
+ 
+    // Add labeled rects for each birthyear.
+    var years = body.selectAll("g")
+	.data(d3.range(min_year - max_age, max_year + 5, 5))
+	.enter().append("g")
+        .attr("transform", function(d) { return "translate(" + x(max_year - d) + ",0)"; });
 
     years.selectAll("rect")
-        .data(function(d) { return data[year][d] || [0, 0]; })
-      .transition()
-        .duration(750)
-        .attr("height", y);
-  }
+	.data(d3.range(2))
+	.enter().append("rect")
+	.attr("x", 1)
+	.attr("width", x(5) - 2)
+	.attr("height", 1e-6);
+
+
+    redraw();
+
+    function redraw() {
+	title.text(max_year);
+
+	body.transition()
+            .duration(750)
+            .attr("transform", function(d) { return "translate(" + x(min_year - max_year) + ",0)"; });
+	
+	years.selectAll("rect")
+            .data(function(d) { return data[year][d] || [0, 0]; })
+	    .transition()
+            .duration(750)
+            .attr("height", y);
+
+    }
 });
+
+
