7d3a95bb636e48aa06b59b2e0aa3e7d5b125a26f
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-16 16:14:18 -0700

    renaming stuff

---
+++ rsload.js
@@ -0,0 +1,12 @@
+var region_load_report = 
+    [
+	{"hostname":"rs1","regions":11},
+	{"hostname":"rs2","regions":7},
+	{"hostname":"rs3","regions":2},
+	{"hostname":"rs4","regions":3}
+    ];
+
+bar_chart("#rsload", region_load_report);
+
+
+
