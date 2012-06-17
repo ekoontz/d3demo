16f7ec3fec98341ff4eeb9e056494c4d2eefa1f0
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-16 17:01:59 -0700

    abstract out HBase specifics from bar chart implementation

--- bar.js
+++ bar.js
@@ -1,45 +1,45 @@
-function bar_chart(dom_id,mydata) {
-
-    var region_load_report = mydata;
+// datum_to_x is a function that takes a single data item (a datum) and returns its x value.
+// datum_to_y is a function that takes a single data item (a datum) and returns its y value.
+function bar_chart(dom_id,data,datum_to_x,datum_to_y) {
 
     // TODO: make these depend on the client's browser window dimensions.
     var bar_chart_height = 200;
     var bar_chart_width = 550;
     
-    var max_value = d3.max(region_load_report, function(regionserver) { return regionserver.regions});
+    var max_value = d3.max(data, function(datum) { return datum_to_y(datum)+0;});
     var vertical_scaling = bar_chart_height / max_value;
-    var bar_width = bar_chart_width / (region_load_report.length * 1.05);
+    var bar_width = bar_chart_width / (data.length * 1.05);
     var max_height = vertical_scaling * max_value;
     
     var chart = d3.select(dom_id).append("svg")
 	.attr("class", "chart")
-	.attr("width", bar_width * region_load_report.length)
+	.attr("width", bar_width * data.length)
 	.attr("height", max_height);
     
-    var x = function(regionserver, i) { return i * bar_width; }
-    var y = function(regionserver) { return max_height - (vertical_scaling * regionserver.regions);}
+    var x = function(datum, i) { return i * bar_width; }
+    var y = function(datum) { return max_height - (vertical_scaling * datum_to_y(datum))};
     
-    // 1. show bars.
+    // 1. show bar for each datum.
     chart.selectAll("rect")
-	.data(region_load_report)
+	.data(data)
 	.enter().append("rect")
 	.attr("x", x)
 	.attr("y", y)
-	.attr("height", function(regionserver, i) { return regionserver.regions * vertical_scaling; })
+	.attr("height", function(datum) { return datum_to_y(datum) * vertical_scaling; })
 	.attr("width", bar_width);
     
-    // 2. label each bar with the hostname of that regionserver.
+    // 2. label each bar with the x value for each datum.
     chart.selectAll("text")
-	.data(region_load_report)
+	.data(data)
 	.enter().append("text")
-	.attr("x", function(regionserver, i) { return ((i) * (bar_width)); })
-	.attr("y", function(regionserver) { return  max_height - 10;})
+	.attr("x", function(datum, i) { return ((i) * (bar_width)); })
+	.attr("y", function(datum) { return  max_height - 10;})
 	.attr("dx", 50)
 	.attr("dy", ".35em") // vertical-align: middle
 	.attr("text-anchor", "right")
-	.text(function(regionserver){ return regionserver.hostname; });
+	.text(function(datum){ return datum_to_x(datum);});
     
-    // 3. ticks
+    // 3. ticks.
     var tickgen = d3.scale.linear()
 	.domain([0,max_value])
     
@@ -48,7 +48,7 @@ function bar_chart(dom_id,mydata) {
 	.data(tick_ys).enter()
 	.append("line")
 	.attr("x1",function(tick) { return 0;})
-	.attr("x2",function(tick) { return bar_width * region_load_report.length;})
+	.attr("x2",function(tick) { return bar_width * data.length;})
 	.attr("y1",function(tick) { return tick * vertical_scaling;})
 	.attr("y2",function(tick) { return tick * vertical_scaling;})
 	.style("stroke","#aaa");
@@ -65,7 +65,7 @@ function bar_chart(dom_id,mydata) {
 	.data(tick_ys)
 	.enter().append("text")
 	.attr("class", "rule")
-	.attr("x", bar_width * region_load_report.length)
+	.attr("x", bar_width * data.length)
 	.attr("dx", magical_x_label_offset)
 	.attr("y", function(tick) {return bar_chart_height - (tick * vertical_scaling);})
 	.attr("dy", magical_tick_label_number)
@@ -75,7 +75,7 @@ function bar_chart(dom_id,mydata) {
     // 4. bottom line
     chart.append("line")
 	.attr("x1", 0)
-	.attr("x2", bar_width * region_load_report.length)
+	.attr("x2", bar_width * data.length)
 	.attr("y1", max_height)
 	.attr("y2", max_height)
 	.style("stroke", "black");
--- rsload.js
+++ rsload.js
@@ -2,11 +2,18 @@ var region_load_report =
     [
 	{"hostname":"rs1","regions":11},
 	{"hostname":"rs2","regions":7},
-	{"hostname":"rs3","regions":2},
+	{"hostname":"rs3","regions":12},
 	{"hostname":"rs4","regions":3}
     ];
 
-bar_chart("#rsload", region_load_report);
+function regionserver_x(regionserver) {
+    regionserver.hostname;
+}
 
+function regionserver_y(regionserver) {
+    regionserver.regions;
+}
 
-
+bar_chart("#rsload", region_load_report, 
+	  function to_x(regionserver) {return regionserver.hostname;},
+	  function to_y(regionserver) {return regionserver.regions;});
