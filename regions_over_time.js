7673b2c944cd905751e77ed792c83f2cd8cb552a
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-17 19:29:55 -0700

    fix typo

--- regions_over_time.js
+++ regions_over_time.js
@@ -3,7 +3,7 @@ var regionserver_histories = [
     // regionserver 1: a reliable workhorse that has never gone down.
     [5,  5,  6,  6,  7,  7, 8, 7, 8],
 
-    // regionserver 2: the "come-back kid": it came down but miraculously recovered."
+    // regionserver 2: the "come-back kid": it came down but miraculously recovered
     [7,  7,  2,  0,  0,  8, 12, 13, 12],
 
     // regionserver 3: a box that starts off well but sadly, crashes and
