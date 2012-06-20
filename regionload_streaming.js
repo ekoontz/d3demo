bd86751f5b7376e5e68fc4bb81b9b64366fadaf2
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-20 09:25:15 -0700

    show 3 regionservers

--- regionload_streaming.js
+++ regionload_streaming.js
@@ -2,17 +2,19 @@
  * "construct a synthetic (i.e., fake) dataset by random walk"
  * - http://mbostock.github.com/d3/tutorial/bar-2.html
  */
-var t = 1297110663; // start time (seconds since epoch)
+var t = new Date().getTime(); // start time (seconds since epoch)
 var v = 70; // start value (subscribers)
-var data = d3.range(33).map(next); // starting dataset
 
 function next() {
     return {
 	time: ++t,
-	value: v = ~~Math.max(10, Math.min(90, v + 10 * (Math.random() - .5)))
+	value: v = ~~Math.max(5, Math.min(20, v + 10 * (Math.random() - .5)))
     };
 }
 
-//var regionserver_histories = [];
+// data looks like [ {time:t0, value: v0} , {time:t1, value: v1}, ... ]
+var rs0 = d3.range(10).map(next); 
+var rs1 = d3.range(10).map(next);
+var rs2 = d3.range(10).map(next);
 
-//streamgraph("#rsload", regionserver_histories);
+var chart = streamgraph("#rsload", new Array(rs0,rs1,rs2));
