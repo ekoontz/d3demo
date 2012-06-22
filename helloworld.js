ee9e8d8e95f6c96a281498cedf0b8765b82e87f5
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-22 09:02:33 -0700

    color circles

--- helloworld.js
+++ helloworld.js
@@ -13,9 +13,9 @@ function hello_world(dom_id) {
 	.attr("height", 300);
 
     mycircles = [
-	{"r": 50, "x": 100, "y":100},
-	{"r": 25, "x": 150, "y":100},
-	{"r": 15, "x": 175, "y":100},
+	{"r": 50, "x": 100, "y":100, "id": "earth"},
+	{"r": 25, "x": 150, "y":100, "id": "mars"},
+	{"r": 15, "x": 175, "y":100, "id": "moon"},
     ];
 
     var get_x_from_circle = function(circle) {return circle.x;};
@@ -25,5 +25,12 @@ function hello_world(dom_id) {
     chart.selectAll("circle").data(mycircles).enter().
 	append("circle").attr("cx",get_x_from_circle).
 	attr("cy",get_y_from_circle).
-	attr("r",get_radius_from_circle);
+	attr("r",get_radius_from_circle).
+	attr("id",function(c) {return c.id;});
+
+
+    console.info("exiting the stage.");
+
+    chart.selectAll("circle").exit();
+
 }
