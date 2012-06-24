9da43c1e5e431f5082400e7222652115b833faf7
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-24 12:04:47 -0700

    more debug logging

--- helloworld.js
+++ helloworld.js
@@ -6,11 +6,16 @@ var mycircles;
 var state = "BEGIN_STATE";
 var iterations = 0;
 
-var earth = {"r": 50, "x": 100, "y":100, "id": "earth"};
-var mars  = {"r": 35, "x": 150, "y":100, "id": "mars"};
-var moon  = {"r": 15, "x": 175, "y":100, "id": "moon"};
+var earth;
+var mars;
+var moon;
 
 var data_array;
+
+var simple;
+var olddata;
+var newdata;
+
 function hello_world(dom_id) {
     state = state_transition(state);
 
@@ -22,33 +27,56 @@ function hello_world(dom_id) {
 
     console.log("Setup of the SVG chart done.");
 
-    var foo = d3.select("#foo");
+    // Simplest selector case: just an array.
+    simple = d3.select("#simple");
 
-    var olddata = foo.selectAll("div").data([4,8,15,16,23,42]);
+    olddata = simple.selectAll("div").data([4,  8,  15, 16,  23,  42]);
+    // After this, the html appearance is [4,8,15,16,23,42]
     olddata.enter().append("div").text(function(d) {return d;});
 
-    var newdata = foo.selectAll("div").data([1,2,4,8,16,32],
-					    function(d) {return d;});
-    // removes things in foo that are not in newdata (namely: 15,23, and 42).
+    // After this, the html appearance is [4,8,15,16,23,42].
+    html_appearance(simple);
+
+    newdata = simple.selectAll("div").data([1,  2,  4,   8,  16,  32],
+					function(d) {return d;});
+
+    // Adds things that are not in olddata but in newdata.
+    // (namely: 1,2,32).
     newdata.enter().append("div").text(function(d) {return d;});
+
+    // After this, the html appearance is [4,8,15,16,23,42,1,2,32].
+    html_appearance(simple);
+
+    // Removes the things that are in olddata but not newdata.
+    // (namely: 15,23,42).
     newdata.exit().remove();
 
+    // After this, the html appearance is [4,8,16,1,2,32].
+    html_appearance(simple);
+
+
+
+
+
+
+    earth = {"r": 50, "x": 100, "y":100, "id": "earth"};
+    mars  = {"r": 35, "x": 150, "y":85, "id": "mars"};
+    moon  = {"r": 15, "x": 175, "y":65, "id": "moon"};
+   
     mycircles = [earth,mars,moon];
 
-    var get_x_from_circle = function(circle) {return circle.x;};
-    var get_y_from_circle = function(circle) {return circle.y;};
-    var get_radius_from_circle = function(circle) {return circle.r;};
-
-    data_array = chart.selectAll("circle").data(mycircles);
+    data_array = chart.selectAll("circle").data(mycircles,
+						function(d) {return d.id;});
 
     data_array.enter().append("circle").
-	attr("cx",function(c){return c.x;}).
-	attr("cy",function(c){return c.y;}).
-        attr("r",function(c) {return c.r;}).
+	attr("cx",function(c) {return c.x;}).
+	attr("cy",function(c) {return c.y;}).
+        attr("r", function(c) {return c.r;}).
         attr("id",function(c) {return c.id;});
 
     console.info("start state: " + state);
 
+    return;
     setInterval(function() {
 	if (state == "MOVE_EARTH") {
 	    if (iterations < 3) {
@@ -62,16 +90,22 @@ function hello_world(dom_id) {
 	    if (state == "MOON_EXIT") {
 		// remove everything except earth and mars.
 		chart.selectAll("circle").data([earth,mars]).exit().remove();
-
 		state = state_transition(state);
 	    } else {
 		if (state == "MOVE_MARS") {
-		    if (iterations < 10) {
+		    if (iterations < 3) {
 			move_planet(d3.select("#mars"));
 			iterations++;
 		    } else {
 			state = state_transition(state);		    
 		    }
+		} else {
+		    if (state == "MOON_REAPPEAR") {
+			console.info("MOON WILL REAPPEAR NOW AS IF BY MAGIC.");
+			chart.selectAll("circle").data([moon]).enter().append();
+			state = state_transition(state);		    
+		    }
+
 		}
 	    }
 	}
@@ -112,10 +146,23 @@ function state_transition(state) {
     if (state == "MOON_EXIT") {
 	return log_state_transition("MOVE_MARS");
     }
+    if (state == "MOVE_MARS") {
+	return log_state_transition("MOON_REAPPEAR");
+    }
+    if (state == "MOON_REAPPEAR") {
+	return log_state_transition("FINAL_STATE");
+    }
     return log_state_transition("FINAL_STATE");
 }
 
 function log_state_transition(state) {
     console.log("moving to state: " + state);
     return state;
+}
+
+// get the innerHTML of every <div> in the given dom_node and return as a string.
+function html_appearance(dom_node) {
+    console.log("div appearance: " + dom_node.selectAll("div")[0].
+		map(function(d) {return d.innerHTML;}));
+
 }
