eb3e853fbcb81c9513e9293c01654a26a0aa1571
Eugene Koontz <ekoontz@hiro-tan.org>
2012-07-03 09:41:12 -0700

    exit transitions working

--- helloworld.js
+++ helloworld.js
@@ -18,58 +18,42 @@ function hello_world(dom_id) {
 
     console.log("Setup of the SVG chart done.");
 
-    // 1. Simplest selector case: just an array.
-    simple = d3.select("#simple");
-
-    update_divs(simple,[4,8,15,16,23,42],
-		function(d) {return d;},
-		function(d) {return d;});
-		
-
-    update_divs(simple,[1,2,4,8,16,32],
-		function(d) {return d;},
-		function(d) {return d;});
-
-    update_divs(simple,[8,16],
-		function(d) {return d;},
-		function(d) {return d;});
-
-    update_divs(simple,[1,2,3,16], 
-		function(d) {return d;},
-		function(d) {return d;});
-
     // 2. More complex case: maps.
     animals = [
 	{"name":"bear",
 	 "age":10,
-	 "animal_id":123
+	 "animal_id":123,
+	 "x":25
+	},
+	{"name":"otter",
+	 "age":15,
+	 "animal_id":1234,
+	 "x":85
 	},
 	{"name":"cat",
 	 "age":20,
-	 "animal_id":456
+	 "animal_id":456,
+	 "x":150
 	},
 	{"name":"dog",
 	 "age":30,
-	 "animal_id":789
+	 "animal_id":789,
+	 "x":210
 	}];
 
-    // bear and cat are friends.
-    friends = [ animals[0], animals[1] ];
+    // bear and cat and otter are friends.
+    friends = [ animals[0], animals[1] ,animals[2] ];
 
     // cat and dog are family.
-    family =  [ animals[1], animals[2] ];
+    family =  [ animals[2], animals[3] ];
 
-    update_divs(d3.select("#structs"),
+    var svg = d3.select("#simple_svg").append("svg")
+	.attr("class", "chart")
+	.attr("width", 500)
+	.attr("height", 100);
 
-		friends,
-
-		// index_fn: what key to use to compare items for equality.
-		function(d) {return d.animal_id;},  
-
-		// text_fn: what to display in the output div.
-		function(d) {return d.name;}); 
-
-    update_svg(d3.select("#simple_svg"),
+    // first show friends.
+    update_svg(svg,
 	       
 	       friends,
 	       
@@ -77,193 +61,62 @@ function hello_world(dom_id) {
 	       function(d) {return d.animal_id;},  
 	       
 	       // text_fn: what to display in the output SVG circle.
-	       function(d) {return d.name;},
-	       
-	       // where on x dimension to start the rows of circles.
-	       50,
-	       // where on x dimension to start the labels.
-	       50);
+	       function(d) {return d.name;});
 
-    update_svg(d3.select("#simple_svg"),
+    // then show friends.
+    update_svg(svg,
 	       
 	       family,
 	       
 	       // index_fn: what key to use to compare items for equality.
-	       function(d) {return d.animal_id;},  
+		   function(d) {return d.animal_id;},  
 	       
 	       // text_fn: what to display in the output SVG circle.
-	       function(d) {return d.name;},
+		   function(d) {return d.name;}
+	      );
+
+    // then just cat.
+    if (false) {
+    update_svg(svg,
 	       
-	       // where on x dimension to start the rows of circles.
-	       50,
-	       // where on x dimension to start the labels.
-	       50);
-
-
-    update_divs(d3.select("#structs"),
-		
-		family,
-		
-		// index_fn: what key to use to compare items for equality.
-		function(d) {return d.animal_id;},
-		
-		// text_fn: what to display in the output div.
-		function(d) {return d.name;},
-		50,
-		50);
-
-    // 3. selectors as applied to SVG. 
-    state = state_transition(state);
-
-    chart = d3.select(dom_id).append("svg")
-	.attr("class", "chart")
-	.attr("width", 500)
-	.attr("height", 300);
-
-    earth = {"r": 50, "x": 100, "y":100, "id": "earth"};
-    mars  = {"r": 35, "x": 150, "y":85, "id": "mars"};
-    moon  = {"r": 15, "x": 175, "y":65, "id": "moon"};
-   
-    mycircles = [earth,mars,moon];
-
-    var data_array = chart.selectAll("circle").data(mycircles,
-						function(d) {return d.id;});
-
-    data_array.enter().append("circle").
-	attr("cx",function(c) {return c.x;}).
-	attr("cy",function(c) {return c.y;}).
-        attr("r", function(c) {return c.r;}).
-        attr("id",function(c) {return c.id;});
-
-    console.info("start state: " + state);
-
-    return;
-    setInterval(function() {
-	if (state == "MOVE_EARTH") {
-	    if (iterations < 3) {
-		move_planet(d3.select("#earth"));
-		iterations++;
-	    } else {
-		state = state_transition(state);
-		iterations = 0;
-	    }
-	} else {
-	    if (state == "MOON_EXIT") {
-		// remove everything except earth and mars.
-		chart.selectAll("circle").data([earth,mars]).exit().remove();
-		state = state_transition(state);
-	    } else {
-		if (state == "MOVE_MARS") {
-		    if (iterations < 3) {
-			move_planet(d3.select("#mars"));
-			iterations++;
-		    } else {
-			state = state_transition(state);		    
-		    }
-		} else {
-		    if (state == "MOON_REAPPEAR") {
-			console.info("MOON WILL REAPPEAR NOW AS IF BY MAGIC.");
-			chart.selectAll("circle").data([moon]).enter().append();
-			state = state_transition(state);		    
-		    }
-
-		}
-	    }
-	}
-	
-    }, 1500);
+	       [ animals[2] ],
+	       
+	       // index_fn: what key to use to compare items for equality.
+		   function(d) {return d.animal_id;},  
+	       
+	       // text_fn: what to display in the output SVG circle.
+		   function(d) {return d.name;}
+	      );}
 
 }
 
-
-function move_planet(planet) {
-    planet.
-	transition().
-	duration(1000)
-	.attr("r", function(d) { 
-	    return (d.r += 2);
-	    
-	})
-	.attr("cy", function(d) { 
-	    d.y = d.y - 5;
-	    return d.y;
-	    
-	})
-	.attr("cx", function(d) { 
-	    d.x = d.x + 5;
-	    return d.x;
-	});
-
-}
-
-function state_transition(state) {
-    console.log("moving from state: " + state);
-    if (state == "BEGIN_STATE") {
-	return log_state_transition("MOVE_EARTH");
-    }
-    if (state == "MOVE_EARTH") {
-	return log_state_transition("MOON_EXIT");
-    }
-    if (state == "MOON_EXIT") {
-	return log_state_transition("MOVE_MARS");
-    }
-    if (state == "MOVE_MARS") {
-	return log_state_transition("MOON_REAPPEAR");
-    }
-    if (state == "MOON_REAPPEAR") {
-	return log_state_transition("FINAL_STATE");
-    }
-    return log_state_transition("FINAL_STATE");
-}
-
-function log_state_transition(state) {
-    console.log("moving to state: " + state);
-    return state;
-}
-
-
-function update_svg(dom_node, newdata_array, index_fn,
-		    text_fn, spread_x,spread_x_label) {
-    console.log("update_svg:spx:" + spread_x);
-    var current_x = spread_x;
-
-    var svg = dom_node.append("svg")
-	.attr("class", "chart")
-	.attr("width", 500)
-	.attr("height", 100);
-
+function update_svg(svg, newdata_array, index_fn,
+		    text_fn) {
     var newdata = svg.selectAll("circle").data(newdata_array,index_fn);
 
     // Add items unique to input_data.
     newdata.enter().append("circle").
 	attr("cx",function(c) {
-	    console.log("spread_x:" + spread_x);
-	    var retval = spread_x;
-	    spread_x = spread_x + 100; 
-	    return retval;
+	    return c.x;
 	}).
 	attr("cy",function(c) {return 45;}).
-        attr("r", function(c) {return 35;}).
-        attr("id",function(animal) {return animal.name + spread_x;});
-
-
+        attr("r", function(c) {return 25;});
+    
     var newlabels = svg.selectAll("text").data(newdata_array,index_fn);
     newlabels.enter().append("text").
 	attr("x",function(c) {
-	    console.log("spread_x_label:" + spread_x_label);
-	    var retval = spread_x_label;
-	    spread_x_label = spread_x_label + 100;
-	    return retval;}).
+	    return c.x;}).
 	attr("y",function(c) {return 45;}).
-        attr("r", function(c) {return 35;}).
-	text(function(animal) {return animal.name;});
+        attr("r", function(c) {return 25;}).
+	text(text_fn);
 
 
     // Remove items not in new data.
-/*    newdata.exit().transition()
-	.duration(5000)
-	.attr("x",500).remove();*/
-    newdata.exit().remove();
+    newdata.exit().transition().duration(3000).attr("cy",-100).remove();
+
+
+    // Remove items not in new data.
+    newlabels.exit().transition().duration(3000).attr("y",-100).remove();
 
     console.log("/update_svg.");
 }
