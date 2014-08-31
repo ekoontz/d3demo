000ea778268032cdc63efa388c78dcbf7943d402
Eugene Koontz <ekoontz@hiro-tan.org>
2014-08-31 10:29:01 -0700

    finally working

--- helloworld.js
+++ helloworld.js
@@ -4,9 +4,10 @@
 var background = "white";
 
 var radius = 35;
+// TODO: get width and height of #game from DOM, not hardcoded.
 var game_width = 1000;
 var game_height = 500;
-var offset=25;
+var offset=0;
 var animals = [
     {"name":"bear",
      "x":50+offset
@@ -36,50 +37,32 @@ var animals = [
 
 var bear  = animals[0];
 var cat   = animals[1];
-var otter = animals[2];
+var cow   = animals[2];
 var dog   = animals[3];
-var wolf  = animals[4];
-var snake = animals[5];
-var cow   = animals[6];
-var gecko = animals[7];
-
-// bear and cat.
-var friends = [ animals[0], animals[1] ,animals[2] ];
-
-// dog and cat.
-var family =  [ animals[2], animals[3] ];
-
-// dog and wolf
-var canine = [ animals[3], animals[4] ];
-
-// bear and otter and wolf and snake.
-var wild =  [ animals[0], animals[1] , animals[4], animals[5], animals[7] ];
-
-var mammals = [ animals[0], animals[1], animals[2], animals[3], animals[4], animals[6] ];
-
-var reptiles = [ animals[5],animals[7]];
-
-var pets = [animals[2],animals[3],animals[5],animals[7]];
+var gecko = animals[4];
+var otter = animals[5];
+var snake = animals[6];
+var wolf  = animals[7];
 
 var set_of_maps = [ {"name":"friends",
-		     "animals":[ bear, otter, cat ]},
+		     "animals":[ bear, cat, otter]},
 		    {"name":"family",
 		     "animals": [ dog,  cat ]},
 		    {"name":"canine",
-		     "animals": [ wolf, dog ]},
+		     "animals": [ dog, wolf ]},
 		    {"name":"wild",
-		     "animals":[ bear, otter, wolf, snake ]},
+		     "animals":[ bear, otter, snake,wolf ]},
 		    {"name":"mammals",
-		     "animals": [bear, otter, cat, dog, wolf, cow ]},
+		     "animals": [ bear, cat, cow, dog, otter, wolf]},
 		    {"name":"reptiles",
-		     "animals":[snake, gecko]},
+		     "animals":[ gecko, snake]},
 		    {"name":"pets",
-		     "animals":[dog, cat, snake, cow]}];
+		     "animals":[ dog, cat, snake]}];
 
 function random_set() {
     var choice_i = Math.floor(Math.random()*(set_of_maps.length));
     var set_name = set_of_maps[choice_i].name;
-    d3.select("#status").html("New set chosen: " + set_name);
+    d3.select("#status").html("New set chosen: " + set_name + " with: " + set_of_maps[choice_i].animals.map(function(a) {return a.name;}));
     return set_of_maps[choice_i];
 }
 
@@ -97,14 +80,11 @@ function keys(arg) {
 
 function show_animal_set(svg) {
     // index_fn: what key to use to compare items for equality.
-    var index_fn = function(d) {return d.animal_id;};
+    var index_fn = function(d) {return d.name;};
     // show the next set in animal_sets.
     var animal_set = random_set();
-//    console.log("group name:" + animal_set.name);
-//    console.log("animal set:" + animal_set);
-//    console.log("animal keys:" + keys(animal_set));
-    console.log("new animal set:" + animal_set.animals.map(function(e) {return e.name;}));
-    update_svg(svg,animal_set,index_fn);
+    console.log("new animal set:" + animal_set.name + "(" + animal_set.animals.map(function(e) {return e.name;}) + ")");
+    update_svg(svg,animal_set.animals,index_fn);
 	
 }
 var existing = null;
@@ -121,30 +101,36 @@ function find_animal(needle,haystack) {
 
 function complement(existing,newset) {
     var keep = [];
-    var to_add = newset;
+    var to_add = [];
     var i;
-    if (existing != null) {
-	to_add = [];
-	console.log("existing   :" + existing.map(function(d) {return d.name + "/" + d.animal_id;}));
-	for(i = 0; i < existing.length; i++) {
-	    console.log("checking for needle: " + existing[i] + " in haystack: " + newset);
-	    if (find_animal(existing[i],newset)) {
-		keep.push(existing[i]);
-	    }
-	}
+    if (existing == null) {
+	return newset;
+    } 
 
-	for(i = 0; i < newset.length; i++) {
-	    console.log("checking for needle: " + newsset[i] + " in haystack: " + exiting);
-	    if (find_animal(newset[i],existing) == false) {
-		to_add.push(newset[i]);
-	    }
+    console.log("complement: existing set: " + existing.map(function(a){return a.name;}));
+    for(i = 0; i < existing.length; i++) {
+	if (find_animal(existing[i],newset)) {
+	    console.log("keeping existing: " + existing[i].name + " since it is also in the new set.");
+	    keep.push(existing[i]);
+	} else {
+	    console.log("removing existing animal:" + existing[i].name);
 	}
+    }
+    for(i = 0; i < newset.length; i++) {
+	if (find_animal(newset[i],existing) == false) {
+	    console.log("introducing new animal:" + newset[i].name);
+	    to_add.push(newset[i]);
+	    } else {
+		console.log(newset[i].name + " is already in set - don't re-add.");
+	    }
     }
 
     var retval = [];
+
     for(i = 0; i < keep.length; i++) {
 	retval.push(keep[i]);
     }
+
     for(i = 0; i < to_add.length; i++) {
 	retval.push(to_add[i]);
     }
@@ -153,21 +139,26 @@ function complement(existing,newset) {
 }
 
 function update_svg(svg, newdata_array, index_fn) {
-//    console.log("new group:" + 
-//		newdata_array.name);
-    var newdata_array = complement(existing,newdata_array.animals,svg.selectAll("circle"));
-    var newdata = svg.selectAll("circle").data(newdata_array,index_fn);
+    if (existing) {
+	console.log("existing:" + 
+		    existing.map(function(a){return a.name;}));
+    }
+    console.log("new group:" + 
+		newdata_array.map(function(a){return a.name;}));
+
+    var introduce = complement(existing,newdata_array,svg.selectAll("circle"));
+    console.log("introducing:" + 
+		introduce.map(function(a){return a.name;}));
+    var newdata = svg.selectAll("circle").data(introduce,index_fn);
 
     // Add items unique to input_data.
     newdata.enter().append("circle").
 	attr("cx",function(c) {
-//	    console.log("appending: " + c.name);
 	    return c.x;
 	}).
 	attr("cy",function(c) {return -50;}).
         attr("r", function(c) {return radius;}).
 	attr("class",function(c) {
-//	    console.log("adding: " + c.name);
 	    return c.name;
 	}).
 	transition().duration(1000).
@@ -178,9 +169,8 @@ function update_svg(svg, newdata_array, index_fn) {
         .style("fill",background)   // fill to white: fade to background
 	.attr("cy",
 	      function(animal) {
-//		  console.log("removing:" + animal.name);
 		  return 800;
 	      }).remove();
 
-    existing = newdata_array.animals;
+    existing = newdata_array;
 }
